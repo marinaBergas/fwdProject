040e54f2fc2ebe1cf020f93cc06c7ce3d54b0137
marinaBergas <marina.bergas@gmail.com>
2022-07-21 14:23:22 +0200

    flex filename

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,32 @@ const app = express();
 const port = 4000;
 const path = require("path");
 
- const imageSrc=path.join(__dirname, "./assets/icelandwaterfall.jpg");
+//  const imageSrc=path.join(__dirname, "./assets/icelandwaterfall.jpg");
 
-
+const imagePath=(filename:string):string=>{
+  
+  const exten=['png','jpg','jpeg'];
+  const imageSrc=path.join(__dirname, `./assets/${filename}.jpg`);
+return imageSrc;
+}
 
 const sharp = require("sharp");
   app.get(
     "/image/:filename?:width?:height?",
     (req: express.Request, res: express.Response) => {
-      const image = res.sendFile(imageSrc);
-
       let width: string = req.query.width as string;
       let height: string = req.query.height as string;
       let filename: string = req.query.filename as string;
+      const imageSrc=imagePath(filename);
+      const image = res.sendFile(imageSrc);
+
       transformImage(width,height,filename);
   
     }
   );
 
 const transformImage=async (width:string,height:string,filename:string)=>{
+  const imageSrc=imagePath(filename);
    await sharp(imageSrc)
   .resize(parseInt(width), parseInt(height))
   .toFile(`src/thumb/${filename}_thumb.jpg`, (error: unknown, info: unknown) => {
@@ -34,6 +41,5 @@ const transformImage=async (width:string,height:string,filename:string)=>{
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
-// uploadImage()
 
 export default {app,transformImage};
